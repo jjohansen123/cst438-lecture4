102cecfc42125462da292e07529ecf8e462571f2
Joakim Johansen <jjohansen@csumb.edu>
2017-09-06 01:23:37 +0000

    ups?2

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'); 
-
-var server = http.createServer(function(request, response){
-    console.log ("Received a request");
-    response.write("<body style = 'background:yellow'>Hurray</body"); 
-    response.end(); 
-}); 
-
-server.listen(8080); 
