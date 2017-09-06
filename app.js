f9580564fba8b2d0a6dabbb099c583bccc850c38
Joakim Johansen <jjohansen@csumb.edu>
2017-09-06 01:49:40 +0000

    changed template

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//console.log("port: " + process.env.PORT); 
+//gives port 8080
+
 app.use('/', index);
 app.use('/users', users);
 
